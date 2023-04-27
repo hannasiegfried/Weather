c9c70835ee42227eeca7aa62403956a9334b9d4a
Ivan Moore <ivan@teamoptimization.com>
2023-04-27 17:15:12 +0100

    minor refactor

--- src/test/java/com/oocode/CachingForecasterTest.java
+++ src/test/java/com/oocode/CachingForecasterTest.java
@@ -2,27 +2,29 @@ package com.oocode;
 
 import org.junit.Test;
 
-import java.time.DayOfWeek;
 import java.util.Random;
 
+import static java.time.DayOfWeek.FRIDAY;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class CachingForecasterTest {
+    private final Random random = new Random();
+
     @Test
     public void delegatesIfForecastNotSeenBefore() {
         Forecaster forecaster = mock(Forecaster.class);
-        Forecast expected = new Forecast(new Random().nextInt(),
-                new Random().nextInt(),
-                "description-" + new Random().nextInt());
-        given(forecaster.forecastFor(DayOfWeek.FRIDAY, "Somewhere"))
+        Forecast expected = new Forecast(random.nextInt(),
+                random.nextInt(),
+                "description-" + random.nextInt());
+        given(forecaster.forecastFor(FRIDAY, "Somewhere"))
                 .willReturn(expected);
 
         CachingForecaster underTest = new CachingForecaster(forecaster);
 
-        Forecast actual = underTest.forecastFor(DayOfWeek.FRIDAY, "Somewhere");
+        Forecast actual = underTest.forecastFor(FRIDAY, "Somewhere");
         assertThat(actual, equalTo(expected));
     }
 }
