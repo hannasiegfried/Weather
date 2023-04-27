7d6b0c0f0c03e4cf775bc46fe097dec1ea8fc416
Ivan Moore <ivan@teamoptimization.com>
2023-04-27 17:26:23 +0100

    refactoring test

--- src/test/java/com/oocode/CachingForecasterTest.java
+++ src/test/java/com/oocode/CachingForecasterTest.java
@@ -12,38 +12,46 @@ import static org.mockito.Mockito.*;
 
 public class CachingForecasterTest {
     private final Random random = new Random();
+    private final Forecaster delegate = mock(Forecaster.class);
+    private final CachingForecaster underTest = new CachingForecaster(delegate);
 
     @Test
     public void delegatesIfForecastNotSeenBefore() {
-        Forecaster forecaster = mock(Forecaster.class);
-        Forecast expected = new Forecast(random.nextInt(),
-                random.nextInt(),
-                "description-" + random.nextInt());
-        DayOfWeek dayOfWeek = DayOfWeek.of(1 + random.nextInt(7));
-        String place = "place-" + random.nextInt();
-        given(forecaster.forecastFor(dayOfWeek, place)).willReturn(expected);
-
-        CachingForecaster underTest = new CachingForecaster(forecaster);
+        Forecast expected = randomForecast();
+        DayOfWeek dayOfWeek = randomDayOfWeek();
+        String place = randomPlace();
+        given(delegate.forecastFor(dayOfWeek, place)).willReturn(expected);
 
         Forecast actual = underTest.forecastFor(dayOfWeek, place);
+
         assertThat(actual, equalTo(expected));
     }
 
     @Test
     public void doesNotAskDelegateAgainIfSeenBefore() {
-        Forecaster forecaster = mock(Forecaster.class);
-        Forecast expected = new Forecast(random.nextInt(),
-                random.nextInt(),
-                "description-" + random.nextInt());
-        DayOfWeek dayOfWeek = DayOfWeek.of(1 + random.nextInt(7));
-        String place = "place-" + random.nextInt();
-        given(forecaster.forecastFor(dayOfWeek, place)).willReturn(expected);
-
-        CachingForecaster underTest = new CachingForecaster(forecaster);
+        Forecast expected = randomForecast();
+        DayOfWeek dayOfWeek = randomDayOfWeek();
+        String place = randomPlace();
+        given(delegate.forecastFor(dayOfWeek, place)).willReturn(expected);
 
         underTest.forecastFor(dayOfWeek, place);
+
         Forecast actual = underTest.forecastFor(dayOfWeek, place);
         assertThat(actual, equalTo(expected));
-        verify(forecaster, times(1)).forecastFor(dayOfWeek, place);
+        verify(delegate, times(1)).forecastFor(dayOfWeek, place);
+    }
+
+    private DayOfWeek randomDayOfWeek() {
+        return DayOfWeek.of(1 + random.nextInt(7));
+    }
+
+    private String randomPlace() {
+        return "place-" + random.nextInt();
+    }
+
+    private Forecast randomForecast() {
+        return new Forecast(random.nextInt(),
+                random.nextInt(),
+                "description-" + random.nextInt());
     }
 }
