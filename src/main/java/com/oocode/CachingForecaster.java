d155c4e9b29bc17ddc40517973987c010d0ae665
Ivan Moore <ivan@teamoptimization.com>
2023-04-27 17:14:26 +0100

    stricter test - using randomness to prevent some of the hard-coding

--- src/main/java/com/oocode/CachingForecaster.java
+++ src/main/java/com/oocode/CachingForecaster.java
@@ -3,11 +3,14 @@ package com.oocode;
 import java.time.DayOfWeek;
 
 public class CachingForecaster implements Forecaster {
+    private final Forecaster forecaster;
+
     public CachingForecaster(Forecaster forecaster) {
+        this.forecaster = forecaster;
     }
 
     @Override
     public Forecast forecastFor(DayOfWeek dayOfWeek, String place) {
-        return new Forecast(1, 2, "Cold");
+        return forecaster.forecastFor(DayOfWeek.FRIDAY, "Somewhere");
     }
 }
--- src/test/java/com/oocode/CachingForecasterTest.java
+++ src/test/java/com/oocode/CachingForecasterTest.java
@@ -3,9 +3,10 @@ package com.oocode;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -13,7 +14,9 @@ public class CachingForecasterTest {
     @Test
     public void delegatesIfForecastNotSeenBefore() {
         Forecaster forecaster = mock(Forecaster.class);
-        Forecast expected = new Forecast(1, 2, "Cold");
+        Forecast expected = new Forecast(new Random().nextInt(),
+                new Random().nextInt(),
+                "description-" + new Random().nextInt());
         given(forecaster.forecastFor(DayOfWeek.FRIDAY, "Somewhere"))
                 .willReturn(expected);
 
