8a31abf30eda5790481f3dd2fca5fe0ad4fe6fe2
Ivan Moore <ivan@teamoptimization.com>
2023-04-28 09:14:53 +0100

    added caching for NavyForecaster adaptor

--- src/main/java/com/oocode/Example.java
+++ src/main/java/com/oocode/Example.java
@@ -8,7 +8,7 @@ public class Example {
             throw new RuntimeException("Must specify Day and Place");
         }
 //        Forecaster forecaster = new CachingForecaster(new MetOfficeForecaster());
-        Forecaster forecaster = new NavyForecaster();
+        Forecaster forecaster = new CachingForecaster(new NavyForecaster());
         forecast(args[0], args[1], forecaster);
         forecast(args[0], args[1], forecaster);
         forecast(args[0], args[1], forecaster);
