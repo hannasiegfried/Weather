86a9bc6c0af6471be38697810dfc90a5d9ed294e
Ivan Moore <ivan@teamoptimization.com>
2024-01-05 17:14:52 +0000

    getting up-to-date - java 21 and junit5 etc

--- src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
+++ src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
@@ -1,9 +1,9 @@
 package com.develogical;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PutYourTestCodeInThisDirectoryTest {
     @Test
